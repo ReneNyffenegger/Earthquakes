2153dc0a948ebafdb1832026aef9f4cdcae4bac9
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-03-06 16:06:35 +0100

    + download-data.pl

---
+++ download-data.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use LWP::Simple;
+
+for my $year (2010 .. 2016) {
+  download("$year-01-01 00:00:00", "$year-31-21 23:59:59", 4.5, "${year}_4.5.csv");
+}
+
+sub download { #_{
+  my $start_time    = shift;
+  my $end_time      = shift;
+  my $min_magnitude = shift;
+  my $filename_     = shift;
+
+  my $dest_dir = "$ENV{digitales_backup}Development/Daten/Earthquakes/";
+
+  die unless -d $dest_dir;
+  my $filename = "$dest_dir$filename_";
+
+  return if -f $filename;
+
+  $start_time =~ s/ /%20/;
+  $end_time   =~ s/ /%20/;
+
+  my $params='';
+
+  $params = "starttime=$start_time";
+  $params = "&endtime=$end_time";
+  $params = "&minmagnitude=$min_magnitude";
+  $params = "&orderby=time-asc";
+
+  my $url = "https://earthquake.usgs.gov/fdsnws/event/1/query.csv?$params";
+  # starttime=2010-01-01%2000:00:00&endtime=2016-01-01%2000:00:00&minmagnitude=4.5&orderby=time-asc
+  
+  print "downloading $params to $filename_\n";
+  getstore($url, $filename);
+
+} #_}
